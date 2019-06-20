e1fdab294fde19c41e2e53aa135f20a565f3d2d4
Felipe Schramm <felipecschramm@hotmail.com>
2019-06-20 02:28:25 -0300

    Updates

---
+++ precache-manifest.8c92c01d3db78dc2366570cc2a40ebed.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40fe79d132502eff210b1d8dd6478b04",
+    "url": "/port/index.html"
+  },
+  {
+    "revision": "e7c3df975539946b893a",
+    "url": "/port/static/css/main.fbd8cda8.chunk.css"
+  },
+  {
+    "revision": "02b90f47a9450f01ebd5",
+    "url": "/port/static/js/2.5147ceb2.chunk.js"
+  },
+  {
+    "revision": "e7c3df975539946b893a",
+    "url": "/port/static/js/main.efa4debf.chunk.js"
+  },
+  {
+    "revision": "2a7ae5c5c38d7d23e3c1",
+    "url": "/port/static/js/runtime~main.a7676607.js"
+  },
+  {
+    "revision": "764661fa842e073b739148ad8a993ba3",
+    "url": "/port/static/media/1.764661fa.png"
+  },
+  {
+    "revision": "1a6b6278d63a14f974ea873e6163649a",
+    "url": "/port/static/media/2.1a6b6278.png"
+  },
+  {
+    "revision": "50c40d840ddc473df2ebbd61fd098112",
+    "url": "/port/static/media/2.50c40d84.webp"
+  },
+  {
+    "revision": "28155b3abfde34dde888bfbf6cf56040",
+    "url": "/port/static/media/3.28155b3a.jpg"
+  },
+  {
+    "revision": "d7cc14a7bda901d45a85337dc9aa021b",
+    "url": "/port/static/media/curriculo.d7cc14a7.pdf"
+  },
+  {
+    "revision": "8f1a518ff69f09062a1e6f1df181ee52",
+    "url": "/port/static/media/g.8f1a518f.jpg"
+  },
+  {
+    "revision": "d8102b233a862b4042d9318990389540",
+    "url": "/port/static/media/mountain3.d8102b23.png"
+  }
+]);
